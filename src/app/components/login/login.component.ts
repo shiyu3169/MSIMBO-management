1a0624f117447a61398b9c5b6d64197789e8d086
Shiyu Wang <shiyu3169@gmail.com>
2018-05-30 15:57:02 -0400

    about and login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var jQuery: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,6 +10,9 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	 jQuery(function () {
+  		jQuery('.my-popover').popover()
+	})
   }
 
 }
