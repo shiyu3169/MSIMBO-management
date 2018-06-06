2cd6e6f83134d75e73309596ce59c4d238373217
Shiyu Wang <shiyu3169@gmail.com>
2018-06-06 13:34:10 -0400

    fix doc and wiki

---
+++ src/app/components/wiki/wiki.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wiki',
+  templateUrl: './wiki.component.html',
+  styleUrls: ['./wiki.component.css']
+})
+export class WikiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
