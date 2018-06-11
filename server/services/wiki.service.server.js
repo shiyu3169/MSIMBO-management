dbaa747270d014b9b13fceb80bf465f7ae9eaf3f
Shiyu Wang <shiyu3169@gmail.com>
2018-06-11 15:02:03 -0400

    install

--- server/services/wiki.service.server.js
+++ server/services/wiki.service.server.js
@@ -3,7 +3,8 @@ module.exports = function (app) {
 	app.get("/api/wiki/:wid", findWikiById);
 
 	var wikis = [
-		{_id: '123', name: 'Angular SPA Structure for Assignment', src: "https://docs.google.com/document/d/e/2PACX-1vQ6g7Y1v4A_vwpo9v_k9bxAyImvAvt_Tt1ZH3hUFf7O56iaB36JRFkYO1qPxUPZv8mAvneRYX3axHcW/pub?embedded=true"}
+		{_id: '123', name: 'Angular SPA Structure for Assignment', src: "https://docs.google.com/document/d/e/2PACX-1vQ6g7Y1v4A_vwpo9v_k9bxAyImvAvt_Tt1ZH3hUFf7O56iaB36JRFkYO1qPxUPZv8mAvneRYX3axHcW/pub?embedded=true"},
+		{_id: '234', name: 'Installation Guide', src: "https://docs.google.com/document/d/e/2PACX-1vTCtUIoIyy1CI_Fjds82uGFVe-QfRCpqIx0jd9zj-5mYcvPo2yynSAgFgA8ta8t3toTsOnLNGVjS1vb/pub?embedded=true"}
 	];
 
 	function findWikis(req, res) {
